608c0a843652d351a04c9cba5d5a0e2f29adf37f
artemshakun <artem.shakun.91@gmail.com>
2022-04-13 17:09:51 +0300

    created cards

--- shop/src/components/Shop.jsx
+++ shop/src/components/Shop.jsx
@@ -1,4 +1,59 @@
+import { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import GoodsList from './GoodsList';
+import Preloader from './Preloader';
+import Cart from './Cart';
+
 function Shop() {
-    return <main className="container content">Shop</main>;
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
+
+    const addToBasket = (item) => {
+        const itemIndex = order.findIndex(
+            (orderItem) => orderItem.id === item.id
+        );
+        if (itemIndex < 0) {
+            const newItem = {
+                ...item,
+                quantity: 1,
+            };
+            setOrder([...order, newItem]);
+        } else {
+            const newOrder = order.map((orderItem, index) => {
+                if (index === itemIndex) {
+                    return {
+                        ...orderItem,
+                        quantity: orderItem.quantity + 1,
+                    };
+                } else {
+                    return item;
+                }
+            });
+            setOrder(newOrder);
+        }
+    };
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                Authorization: API_KEY,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            });
+    }, []);
+    return (
+        <main className="container content">
+            <Cart quantity={order.length} />
+            {loading ? (
+                <Preloader />
+            ) : (
+                <GoodsList goods={goods} addToBasket={addToBasket} />
+            )}
+        </main>
+    );
 }
 export default Shop;
